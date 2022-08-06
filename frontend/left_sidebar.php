a10296775d28591c4f92eafe4257950b4bc2fc81
Vijay Pal Gautam <vijaypal.hestabit@gmail.com>
2022-08-06 16:35:48 +0530

    add video

--- frontend/left_sidebar.php
+++ frontend/left_sidebar.php
@@ -1,10 +1,10 @@
 <div class="row content">
     <div class="col-sm-2 py-4 bg-light">
         <div class="w-100">
-            <video src="../videos/Asia_Cup_2022.mp4" autoplay controls muted loop></video>
+            <video src="../videos/Asia_Cup_2022.mp4" class="w-100" autoplay controls muted loop></video>
         </div>
         <div class="w-100">
-            <video src="../videos/Asia_Cup_2022_2.mp4" autoplay controls muted loop></video>
+            <video src="../videos/Asia_Cup_2022_2.mp4" class="w-100" autoplay controls muted loop></video>
         </div>
     </div>
     
